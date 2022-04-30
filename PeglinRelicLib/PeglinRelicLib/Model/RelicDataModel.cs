ea3735791569af10670f9c58c88a6a9b3c638baa
Jackson Levitt <47047592+crazyjackel@users.noreply.github.com>
2022-04-29 21:18:51 -0400

    Init

---
+++ PeglinRelicLib/PeglinRelicLib/Model/RelicDataModel.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeglinRelicLib.Model
+{
+    public class RelicDataModel
+    {
+        /// <summary>
+        /// Function to Assist in Setting Assembly Path
+        /// </summary>
+        /// <param name="plugin"></param>
+        public void SetAssemblyPath(BaseUnityPlugin plugin)
+        {
+            var assembly = plugin.GetType().Assembly;
+            var uri = new UriBuilder(assembly.CodeBase);
+            AssemblyPath = Path.GetDirectoryName(Uri.UnescapeDataString(uri.Path));
+        }
+        public string AssemblyPath { get; set; }
+        public string BundlePath { get; set; }
+        public string SpriteName { get; set; }
+        public string AudioName { get; set; }
+        public string FullPath => Path.Combine(AssemblyPath, BundlePath);
+
+        public string LocalKey { get; set; }
+        public string DescriptionKey { get; set; }
+    }
+}
---
+++ PeglinRelicLib/PeglinRelicLib/Plugin.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using System.IO;
+using UnityEngine;
+using Relics;
+using I2.Loc;
+using PeglinRelicLib.Model;
+
+namespace PeglinRelicLib
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "io.github.crazyjackel.RelicLib";
+        public const string Name = "crazyjackel";
+        public const string Version = "1.0.0";
+
+        static string m_path;
+        static int m_rg = Enum.GetValues(typeof(RelicEffect)).Cast<int>().Max();
+        static Plugin m_lib;
+        static RelicManager m_relicManager;
+        public static Plugin RelicLib => m_lib;
+        public static string BasePath => m_path;
+        static Plugin()
+        {
+            //Calculate out a BasePath
+            var assembly = typeof(Plugin).Assembly;
+            var uri = new UriBuilder(assembly.CodeBase);
+            m_path = Path.GetDirectoryName(Uri.UnescapeDataString(uri.Path));
+        }
+        void Awake()
+        {
+            if (m_lib != null) Destroy(this);
+
+            m_lib = this;
+            m_relicManager = Resources.FindObjectsOfTypeAll<RelicManager>().First();
+
+            if (m_relicManager == null)
+            {
+                Debug.Log("Relic Manager Not Found");
+                return;
+            }
+
+
+            Harmony patcher =  new Harmony(GUID);
+            patcher.PatchAll();
+        }
+
+
+        public RelicEffect RegisterRelic(RelicDataModel relicData)
+        {
+            Relic r = ScriptableObject.CreateInstance<Relic>();
+            r.locKey = relicData.LocalKey;
+            r.descMod = relicData.DescriptionKey;
+
+            return (RelicEffect)m_rg++;
+        }
+    }
+}
